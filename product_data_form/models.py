47e63c4b230105db31aeeedc408af70b0d6ae197
DR Subordinate <karin.pspink@gmail.com>
2024-01-30 12:43:21 +0900

    modelの作成

--- product_data_form/models.py
+++ product_data_form/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    date = models.DateField()
+    market_name = models.CharField(max_length=200, blank=True)
+    number = models.CharField(max_length=200, blank=True)
+    brand_name = models.CharField(max_length=200, blank=True)
+    name = models.CharField(max_length=200, blank=True)
+    model_number = models.CharField(max_length=200, blank=True)
+    serial_number = models.CharField(max_length=200, blank=True)
+    material_color = models.CharField(max_length=200, blank=True)
+    condition = models.CharField(max_length=200, blank=True)
+    detail = models.TextField(blank=True)
+    price = models.CharField(max_length=200, blank=True)
+    winning_bid = models.CharField(max_length=200, blank=True)
