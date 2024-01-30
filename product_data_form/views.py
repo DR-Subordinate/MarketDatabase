e41511272ae50336785dd3f60b57f895f5899cf7
DR Subordinate <karin.pspink@gmail.com>
2024-01-30 19:25:12 +0900

    フォームの入力を保存できるようにviewを修正

--- product_data_form/views.py
+++ product_data_form/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import ProductForm
 from .models import Product
 
 def index(request):
     if request.method == "POST":
-        form = ProductForm()
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("product_data_form:index")
     else:
         form = ProductForm()
     return render(request, "product_data_form/index.html", {"form": form})
