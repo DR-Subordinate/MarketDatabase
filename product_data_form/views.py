1ffedb1aac4e4eaad9b2555413baa15cc5308406
DR Subordinate <karin.pspink@gmail.com>
2024-02-26 16:07:02 +0900

    全てのフィールドで検索できるような検索機能の実装

--- product_data_form/views.py
+++ product_data_form/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 
 from .forms import ProductForm
 from .models import Product
@@ -15,8 +16,22 @@ def index(request):
 
 def search(request):
     if request.method == "GET":
-        if request.GET.get("search-query"):
-            products = Product.objects.filter(name__icontains=request.GET.get("search-query"))
+        search_query = request.GET.get("search-query")
+        if search_query:
+            products = Product.objects.filter(
+                Q(date__icontains=search_query) |
+                Q(market_name__icontains=search_query) |
+                Q(number__icontains=search_query) |
+                Q(brand_name__icontains=search_query) |
+                Q(name__icontains=search_query) |
+                Q(model_number__icontains=search_query) |
+                Q(serial_number__icontains=search_query) |
+                Q(material_color__icontains=search_query) |
+                Q(condition__icontains=search_query) |
+                Q(detail__icontains=search_query) |
+                Q(price__icontains=search_query) |
+                Q(winning_bid__icontains=search_query)
+            )
         else:
             return render(request, "product_data_form/search.html")
     return render(request, "product_data_form/search.html", {"products": products})
