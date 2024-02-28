cb701cbf020e38379f5ee56e3607daef999f424d
DR Subordinate <karin.pspink@gmail.com>
2024-02-28 15:34:10 +0900

    値段と落札価格の保存をするページのURLの設定を追加

--- product_data_form/urls.py
+++ product_data_form/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "product_data_form"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("price", views.save_price, name="price"),
     path("search/", views.search, name="search"),
 ]
