5160deb3a415195dc9110750b7b807a45ee1d037
DR Subordinate <karin.pspink@gmail.com>
2024-03-19 18:05:34 +0900

    本番環境用のsettings.pyを追加

---
+++ marketdatabase/settings_prod.py
@@ -0,0 +1,22 @@
+import os
+
+from .settings import *
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["marketdb.across-shop.com"]
+
+STATIC_ROOT = BASE_DIR / "static/"
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_PRELOAD = True
