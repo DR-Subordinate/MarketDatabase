2543b0094cb4be86c67ae4cccebbeac2dd9bf2ba
DR Subordinate <karin.pspink@gmail.com>
2024-03-26 20:20:26 +0900

    modelの変更
    市場と商品情報を紐づけるために新しくMarketモデルを作成し
    Productとone-to-manyの関係にする

--- product_data_form/models.py
+++ product_data_form/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+class Market(models.Model):
+    date = models.DateField(blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
-    date = models.DateField(blank=True, null=True)
-    market_name = models.CharField(max_length=200, blank=True)
+    market = models.ForeignKey(Market, on_delete=models.CASCADE, blank=True, null=True)
     number = models.CharField(max_length=200, blank=True)
     brand_name = models.CharField(max_length=200, blank=True)
     name = models.CharField(max_length=200, blank=True)
