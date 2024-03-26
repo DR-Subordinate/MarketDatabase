bc277e5db92be17c5017e1c0da90611ca289a3ea
DR Subordinate <karin.pspink@gmail.com>
2024-03-26 20:21:53 +0900

    fieldsの変更
    modelの変更に合わせてdateとmarket_nameを削除

--- product_data_form/forms.py
+++ product_data_form/forms.py
@@ -4,7 +4,7 @@ from .models import Product
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ["date", "market_name", "number", "brand_name", "name",
+        fields = ["number", "brand_name", "name",
                   "model_number", "serial_number", "material_color",
                   "condition", "detail", "price", "winning_bid"]
         widgets = {
