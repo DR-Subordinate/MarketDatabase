1488e30939a6660766551595e5762ad3c82f75ee
DR Subordinate <karin.pspink@gmail.com>
2024-03-29 19:46:09 +0900

    ProductFormの部品全てにborderを追加

--- product_data_form/forms.py
+++ product_data_form/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, DateInput
+from django.forms import ModelForm, DateInput, TextInput, Textarea
 from .models import Product, Market
 
 class MarketForm(ModelForm):
@@ -15,3 +15,15 @@ class ProductForm(ModelForm):
         fields = ["number", "brand_name", "name",
                   "model_number", "serial_number", "material_color",
                   "condition", "detail", "price", "winning_bid"]
+        widgets = {
+            "number": TextInput(attrs={"class":"border border-black"}),
+            "brand_name": TextInput(attrs={"class":"border border-black"}),
+            "name": TextInput(attrs={"class":"border border-black"}),
+            "model_number": TextInput(attrs={"class":"border border-black"}),
+            "serial_number": TextInput(attrs={"class":"border border-black"}),
+            "material_color": TextInput(attrs={"class":"border border-black"}),
+            "condition": TextInput(attrs={"class":"border border-black"}),
+            "detail": Textarea(attrs={"class":"border border-black"}),
+            "price": TextInput(attrs={"class":"border border-black"}),
+            "winning_bid": TextInput(attrs={"class":"border border-black"})
+        }
