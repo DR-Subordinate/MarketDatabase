8a3b4fe61c1c41032ae53c710a33447ff9e23e18
DR Subordinate <karin.pspink@gmail.com>
2024-04-10 15:28:06 +0900

    Validation Errorを解消するためにスペースを追加
    market_nameとmarket_dateが市場名と日付で
    うまく分かれない不具合が出たため間にスペースを追加

--- product_data_form/urls.py
+++ product_data_form/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "product_data_form"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:market_name><str:market_date>", views.save_product, name="product"),
+    path("<str:market_name> <str:market_date>", views.save_product, name="product"),
     path("price/", views.save_price, name="price"),
     path("search/", views.search, name="search"),
 ]
