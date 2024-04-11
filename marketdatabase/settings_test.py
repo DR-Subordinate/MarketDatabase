553093666f4786c2c70b04b31fb1b0abdd4ae9c0
DR Subordinate <karin.pspink@gmail.com>
2024-04-11 18:17:56 +0900

    テスト環境用の設定ファイル

---
+++ marketdatabase/settings_test.py
@@ -0,0 +1,11 @@
+import os
+
+from .settings import *
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["test-marketdb.across-shop.com/"]
+
+STATIC_ROOT = BASE_DIR / "static/"
