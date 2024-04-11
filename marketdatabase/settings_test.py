225bafabaf27a34af14a7da5097541a2111d764e
DR Subordinate <karin.pspink@gmail.com>
2024-04-11 18:19:57 +0900

    ALLOWED_HOSTSの"/"を削除
    設定ミス

--- marketdatabase/settings_test.py
+++ marketdatabase/settings_test.py
@@ -6,6 +6,6 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["test-marketdb.across-shop.com/"]
+ALLOWED_HOSTS = ["test-marketdb.across-shop.com"]
 
 STATIC_ROOT = BASE_DIR / "static/"
