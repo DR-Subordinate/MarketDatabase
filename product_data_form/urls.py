9ab7c4a1e76fb470b978a8c753d2c984799603cb
DR Subordinate <karin.pspink@gmail.com>
2024-04-21 05:29:54 +0900

    新しいURLを追加
    商品情報編集用のページのURLを追加

--- product_data_form/urls.py
+++ product_data_form/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("<str:market_name> <str:market_date>", views.save_product, name="product"),
     path("price/", views.save_price, name="price"),
     path("search/", views.search, name="search"),
+    path("edit/", views.edit, name="edit"),
 ]
