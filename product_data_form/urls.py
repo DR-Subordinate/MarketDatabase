ed413c3a13c5ae0427f7f97bf0169e002b632e34
DR Subordinate <karin.pspink@gmail.com>
2024-07-30 17:44:17 +0900

    product urlを変更
    productをmainのproduct用に変更

--- product_data_form/urls.py
+++ product_data_form/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = "product_data_form"
 urlpatterns = [
     path("main/", views.main, name="main"),
+    path("main/<str:market_name>_<str:market_date>", views.product_main, name="product_main"),
     path("register/", views.register, name="register"),
     path("", views.index, name="index"),
-    path("<str:market_name>_<str:market_date>", views.save_product, name="product"),
     path("price/", views.save_price, name="price"),
     path("search/", views.search, name="search"),
     path("edit/", views.edit, name="edit"),
