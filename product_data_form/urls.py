6a2c0aaf9860c4aad359632a672e334a4fbdc5cf
DR Subordinate <karin.pspink@gmail.com>
2024-08-20 11:58:01 +0900

    使わなくなったURLを削除

--- product_data_form/urls.py
+++ product_data_form/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path("main/<str:market_name>_<str:market_date>", views.product_main, name="product_main"),
     path("register/<str:market_name>_<str:market_date>", views.product_register, name="product_register"),
     path("", views.index, name="index"),
-    path("price/", views.save_price, name="price"),
     path("search/", views.search, name="search"),
-    path("edit/", views.edit, name="edit"),
 ]
