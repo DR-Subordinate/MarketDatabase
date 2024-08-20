d451be030ebc091f64442e0b26083fa21487e7f4
DR Subordinate <karin.pspink@gmail.com>
2024-08-20 11:59:50 +0900

    index URLのviewをmainに変更
    間違えてこのURLにアクセスしてもmainに飛ばすように設定することによって
    テスト環境で詳細な404ページを表示させないようにする

--- product_data_form/urls.py
+++ product_data_form/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = "product_data_form"
 urlpatterns = [
+    path("", views.main, name="index"),
     path("main/", views.main, name="main"),
     path("main/<str:market_name>_<str:market_date>", views.product_main, name="product_main"),
     path("register/<str:market_name>_<str:market_date>", views.product_register, name="product_register"),
-    path("", views.index, name="index"),
     path("search/", views.search, name="search"),
 ]
