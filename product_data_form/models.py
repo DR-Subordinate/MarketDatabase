94b459bed7654209fad039fff0c4b8af9c3811b4
DR Subordinate <karin.pspink@gmail.com>
2024-09-27 16:13:01 +0900

    Market modelを変更
    市場と紐づけられた請求書を保存するためにinvoice_pdfを追加

--- product_data_form/models.py
+++ product_data_form/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Market(models.Model):
     date = models.DateField()
     name = models.CharField(max_length=200)
+    invoice_pdf = models.FileField(upload_to='invoices/', null=True, blank=True)
 
     def __str__(self):
         return f"{self.name} {self.date}"
