b5feb0026cb8c58158ff26004006ba9c8d0dce0a
DR Subordinate <karin.pspink@gmail.com>
2024-12-20 23:07:20 +0900

    adminにmodelを追加

--- star_buyers_auction/admin.py
+++ star_buyers_auction/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Auction, Product
+
+class ProductInline(admin.StackedInline):
+    model = Product
+    extra = 1
+
+class AuctionAdmin(admin.ModelAdmin):
+    inlines = [ProductInline]
+
+admin.site.register(Auction, AuctionAdmin)
+admin.site.register(Product)
