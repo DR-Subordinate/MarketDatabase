c507ea687cb95a26af457f1e89566e070aae13bb
DR Subordinate <karin.pspink@gmail.com>
2024-12-20 22:58:34 +0900

    modelの作成

--- star_buyers_auction/models.py
+++ star_buyers_auction/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Auction(models.Model):
+    date = models.DateField()
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name} {self.date}"
+
+
+class Product(models.Model):
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, blank=True, null=True)
+    image = models.ImageField(upload_to='star_buyers_auction/', null=True, blank=True)
+    brand_name = models.CharField(max_length=200, blank=True)
+    name = models.CharField(max_length=200, blank=True)
+    ended_at = models.DateField(null=True, blank=True)
+    rank = models.CharField(max_length=200, blank=True)
+    price = models.CharField(max_length=200, blank=True)
+    current_bidding_price = models.CharField(max_length=200, blank=True)
+    memo = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.brand_name} {self.name}"
