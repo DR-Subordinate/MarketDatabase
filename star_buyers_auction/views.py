562a5369662adf20c49d32c59ccf2d88fc0a8999
DR Subordinate <karin.pspink@gmail.com>
2024-12-23 23:17:34 +0900

    スクレイピングを分割する処理を追加
    10個ずつ商品情報を取得する処理を追加
    request.sessionでどこまで処理を終えたかを記録する

--- star_buyers_auction/views.py
+++ star_buyers_auction/views.py
@@ -11,6 +11,8 @@ from .compress_images import compress_sba_images
 def index(request):
     storage_info = get_disk_usage()
     compression_message = None
+    progress_message = None
+
     if storage_info['usage_percent'] > 95:
         compress_sba_images()
         storage_info = get_disk_usage()
@@ -34,32 +36,58 @@ def index(request):
             )
 
             if sba.login():
-                auction = Auction.objects.create(
-                    date=datetime.strptime(end_date, '%Y-%m-%d').date(),
-                    name="スタバイ"
-                )
+                if 'product_links' not in request.session:
+                    auction = Auction.objects.create(
+                        date=datetime.strptime(end_date, '%Y-%m-%d').date(),
+                        name="スタバイ"
+                    )
+                    product_links = sba.collect_product_links()
+                    request.session['product_links'] = product_links
+                    request.session['processing_index'] = 0
+                    end_date = end_date
+                else:
+                    auction = Auction.objects.filter(
+                        date=datetime.strptime(end_date, '%Y-%m-%d').date()
+                    ).last()
 
                 existing_products = set(AuctionProduct.objects.values_list('product_link', flat=True))
+                product_links = request.session['product_links']
+                start_index = request.session['processing_index']
 
-                product_links = sba.collect_product_links()
-                product_data = sba.collect_product_data(product_links)
+                BATCH_SIZE = 10
+                batch_links = product_links[start_index:start_index + BATCH_SIZE]
 
-                for item in product_data:
-                    if item['product_link'] not in existing_products:
-                        image_content, image_name = item['image']
-                        AuctionProduct.objects.create(
-                            auction=auction,
-                            image=ContentFile(image_content, name=image_name) if image_content else None,
-                            brand_name=item['brand_name'],
-                            name=item['product_name'],
-                            ended_at=datetime.strptime(item['ended_at'], '%Y/%m/%d').date(),
-                            rank=item['data_rank'],
-                            price=item['price'],
-                            current_bidding_price=item['current_bidding_price'],
-                            memo=item['memo'],
-                            product_link=item['product_link']
-                        )
-                return redirect('star_buyers_auction:index')
+                if batch_links:
+                    product_data = sba.collect_product_data(batch_links)
+                    for item in product_data:
+                        if item['product_link'] not in existing_products:
+                            image_content, image_name = item['image']
+                            AuctionProduct.objects.create(
+                                auction=auction,
+                                image=ContentFile(image_content, name=image_name) if image_content else None,
+                                brand_name=item['brand_name'],
+                                name=item['product_name'],
+                                ended_at=datetime.strptime(item['ended_at'], '%Y/%m/%d').date(),
+                                rank=item['data_rank'],
+                                price=item['price'],
+                                current_bidding_price=item['current_bidding_price'],
+                                memo=item['memo'],
+                                product_link=item['product_link']
+                            )
+
+                    request.session['processing_index'] = start_index + BATCH_SIZE
+                    progress_message = f"処理中... {start_index + BATCH_SIZE}/{len(product_links)}件"
+
+                    context = {
+                        "storage_info": storage_info,
+                        "compression_message": compression_message,
+                        "progress_message": progress_message,
+                        "continue_processing": True,
+                        "auction_end_date": end_date  # Add this to maintain the form value
+                    }
+                    return render(request, "star_buyers_auction/index.html", context)
+                else:
+                    request.session.flush()
             else:
                 return render(request, "star_buyers_auction/index.html",
                             {"error_message": "ログインに失敗しました。"})
@@ -68,5 +96,9 @@ def index(request):
             return render(request, "star_buyers_auction/index.html",
                         {"error_message": f"エラーが発生しました: {str(e)}"})
 
-    context = {"storage_info": storage_info, "compression_message": compression_message}
+    context = {
+        "storage_info": storage_info,
+        "compression_message": compression_message,
+        "progress_message": progress_message
+    }
     return render(request, "star_buyers_auction/index.html", context)
