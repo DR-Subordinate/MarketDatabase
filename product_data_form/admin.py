0b68b9ee32b35889ffac8981b50be53ea440b755
DR Subordinate <karin.pspink@gmail.com>
2025-01-13 18:55:28 +0900

    adminにInvoicePDFモデルを追加

--- product_data_form/admin.py
+++ product_data_form/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
-from .models import Market, Product
+from .models import Market, Product, InvoicePDF
 
 class ProductInline(admin.StackedInline):
     model = Product
     extra = 1
 
+class InvoicePDFInline(admin.TabularInline):
+    model = InvoicePDF
+    extra = 1
+
 class MarketAdmin(admin.ModelAdmin):
-    inlines = [ProductInline]
+    inlines = [ProductInline, InvoicePDFInline]
 
 admin.site.register(Market, MarketAdmin)
 admin.site.register(Product)
+admin.site.register(InvoicePDF)
