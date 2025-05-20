e4114fbf34b4f666e5da587d89d71ddcfcb5cc9d
DR Subordinate <karin.pspink@gmail.com>
2025-05-20 18:50:17 +0900

    LineUserのモデルの構造をよりシンプルに変更
    LINEの通知を送るのに必要なのはline_user_idだけなので
    よりシンプルな構造に変更した

--- line_notification/models.py
+++ line_notification/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class LineUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    line_user_id = models.CharField(max_length=255, unique=True)
-    is_active = models.BooleanField(default=True)
+    line_user_id = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name or self.line_user_id
