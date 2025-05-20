557f519b8e6c971764ca24657bb08195731e35ee
DR Subordinate <karin.pspink@gmail.com>
2025-05-20 17:37:18 +0900

    LINEの通知機能実装に必要な設定を追加

--- marketdatabase/settings.py
+++ marketdatabase/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'product_data_form.apps.ProductDataFormConfig',
     'star_buyers_auction.apps.StarBuyersAuctionConfig',
+    'line_notification.apps.LineNotificationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -132,3 +134,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
+
+if not load_dotenv(dotenv_path=".env.local"):
+    LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+else:
+    LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
