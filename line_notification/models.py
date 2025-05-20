b7e482d56aa6fa0ed1704a632385d8195fa50964
DR Subordinate <karin.pspink@gmail.com>
2025-05-20 17:17:03 +0900

    LineUserモデルを作成

--- line_notification/models.py
+++ line_notification/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class LineUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    line_user_id = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
