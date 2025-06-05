238871bb568600f90a335328580e23c85e3e57c2
DR Subordinate <karin.pspink@gmail.com>
2025-06-05 15:55:10 +0900

    Viteの設定ファイルを追加

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        // Global CSS
+        main: resolve(__dirname, 'src/style.css'),
+        // Per-app JavaScript
+        product_data_form: resolve(__dirname, 'product_data_form/static/src/main.ts'),
+      },
+      output: {
+        dir: 'static',
+        entryFileNames: `js/[name].js`,
+        assetFileNames: 'css/main.css',
+      }
+    }
+  }
+})
