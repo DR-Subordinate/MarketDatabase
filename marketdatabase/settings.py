b8cbcc3ea9d0d02558791604f16e362d8943cece
DR Subordinate <karin.pspink@gmail.com>
2025-06-05 15:03:36 +0900

    Viteの出力先の設定を追加

--- marketdatabase/settings.py
+++ marketdatabase/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Vite build output
+]
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
