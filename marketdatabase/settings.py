9bde0f40195e412ebd731277efa0dd2cef390d48
DR Subordinate <karin.pspink@gmail.com>
2025-06-06 14:34:12 +0900

    STATIC_URLの設定を元に戻す
    index.cgiを削除

--- marketdatabase/settings.py
+++ marketdatabase/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/index.cgi/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "dist",  # Vite build output
