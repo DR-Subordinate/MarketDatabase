51812e33a742aa4b0404ce9bdf3c3c423704325b
DR Subordinate <karin.pspink@gmail.com>
2025-06-11 18:17:42 +0900

    initProductRegisterのインポートとその実行

--- product_data_form/static/src/main.ts
+++ product_data_form/static/src/main.ts
@@ -1,9 +1,12 @@
 import { initMainPage } from "./pages/main_page";
+import { initProductRegister } from "./pages/product_register";
 
 document.addEventListener("DOMContentLoaded", () => {
   const currentPage = window.location.pathname;
 
   if (currentPage === "/" || currentPage === "/index.cgi/") {
     initMainPage();
+  } else if (currentPage.includes("register")) {
+    initProductRegister();
   }
 });
