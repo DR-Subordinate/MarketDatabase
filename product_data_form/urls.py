c1b1e3bbb78153d61928ed64167f9565ae8b85a1
DR Subordinate <karin.pspink@gmail.com>
2025-06-27 11:31:12 +0900

    ””のURLの名前をmainに変更
    redirect()でproduct_data_form:mainと指定しているので
    ””のURLの名前をmainに変更

--- product_data_form/urls.py
+++ product_data_form/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "product_data_form"
 urlpatterns = [
-    path("", views.main, name="index"),
+    path("", views.main, name="main"),
     path("main/<str:market_name>_<str:market_date>", views.product_main, name="product_main"),
     path("register/<str:market_name>_<str:market_date>", views.product_register, name="product_register"),
     path("search/", views.search, name="search"),
